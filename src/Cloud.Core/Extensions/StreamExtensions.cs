22786a1b93bc61c89ca5de1847cdf3ae1eba834d
Robert McCabe <rmccabe@eshopworld.com>
2020-07-01 05:09:01 +0100

    Added stream extensions

--- src/Cloud.Core/Extensions/StreamExtensions.cs
+++ src/Cloud.Core/Extensions/StreamExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace System.IO
 {
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Extension methods for Stream.
     /// </summary>
@@ -11,13 +13,28 @@ namespace System.IO
         /// </summary>
         /// <param name="input">The input.</param>
         /// <returns>System.Byte[].</returns>
-        public static byte[] CopyToBytes(this Stream input)
+        public static async Task<byte[]> ToBytes(this Stream input)
         {
             using (MemoryStream ms = new MemoryStream())
             {
-                input.CopyTo(ms);
+                if (input.CanSeek)
+                {
+                    input.Position = 0;
+                }
+
+                await input.CopyToAsync(ms);
                 return ms.ToArray();
             }
+        }
+
+        /// <summary>
+        /// Converts the file stream to base64.
+        /// </summary>
+        /// <param name="stream">The stream to convert.</param>
+        /// <returns>s.</returns>
+        public static async Task<string> ToBase64(this Stream stream)
+        {
+            return Convert.ToBase64String(await stream.ToBytes());
         }
     }
 }
--- src/Tests/SteamExtensionsTest.cs
+++ src/Tests/SteamExtensionsTest.cs
@@ -11,7 +11,7 @@ namespace Cloud.Core.Tests
     {
         /// <summary>Check the string is converted into stream and back into string as expected.</summary>
         [Fact]
-        public void Test_Stream_ConvertToFromString()
+        public async void Test_Stream_ConvertToFromString()
         {
             // Arrange - setup stream.
             string testText = "My Test String";
@@ -19,10 +19,28 @@ namespace Cloud.Core.Tests
             byte[] stringBytes = Encoding.UTF8.GetBytes(testText);
 
             // Act - convert steam to bytes for comparison.
-            byte[] streamBytes = textStream.CopyToBytes();
+            byte[] streamBytes = await textStream.ToBytes();
 
             // Assert - ensure both are equal, confirming method has worked.
             Assert.Equal(stringBytes, streamBytes);
+        }
+
+        /// <summary>Convert to base64 and back again.</summary>
+        [Theory]
+        [InlineData("test data text")]
+        [InlineData("other test data")]
+        public void Test_Stream_ConvertToFromBase64(string testString)
+        {
+            // Arrange - setup stream.
+            Stream textStream = testString.ConvertToStream(Encoding.UTF8);
+
+            // Act - convert steam to bytes for comparison.
+            var base64 = textStream.ToBase64().GetAwaiter().GetResult();
+            var bytes = textStream.ToBytes().GetAwaiter().GetResult();
+            var revert = Convert.FromBase64String(base64);
+
+            // Assert - ensure both are equal, confirming method has worked.
+            Assert.Equal(bytes, revert);
         }
     }
 }
