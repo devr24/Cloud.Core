69627e18297c0cf8f8926c1d7e0c7e9003092061
Robert McCabe <rmccabe@eshopworld.com>
2020-07-02 00:43:41 +0100

    Added new extension method

---
+++ src/Cloud.Core/Services/MonitorService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace Cloud.Core.Services
+{
+    /// <summary>Monitor Configuration</summary>
+    public class MonitorConfig
+    {
+        /// <summary>Initializes a new instance of the <see cref="MonitorConfig"/> class.</summary>
+        /// <param name="frequencySeconds">The frequency seconds.</param>
+        public MonitorConfig(int frequencySeconds = 60)
+        {
+            MonitorFrequencySeconds = frequencySeconds;
+        }
+
+        /// <summary>Gets the monitor frequency seconds.</summary>
+        /// <value>The monitor frequency seconds.</value>
+        public int MonitorFrequencySeconds { get; }
+    }
+
+    /// <summary>Monitor Service class tracks a constant tick throughout the application.</summary>
+    public class MonitorService
+    {
+        private readonly ILogger<MonitorService> _logger;
+        private readonly Stopwatch _elapsedTime = new Stopwatch();
+        private readonly MonitorConfig _config;
+
+        /// <summary>Gets the name of the application.</summary>
+        /// <value>The name of the application.</value>
+        public string AppName => AppDomain.CurrentDomain.FriendlyName;
+
+        /// <summary>
+        /// Gets or sets the background timer tick action event - used to hook into the background timer tick to allow custom logs to be written.
+        /// </summary>
+        /// <value>The background timer tick.</value>
+        public Action<TimeSpan> BackgroundTimerTick { get; set; } = null;
+
+        /// <summary>Initializes a new instance of the <see cref="MonitorService"/> class.</summary>
+        public MonitorService()
+        {
+            _config = new MonitorConfig();
+            StartMonitor();
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MonitorService"/> class.</summary>
+        /// <param name="frequencySeconds">The frequency seconds.</param>
+        public MonitorService(int frequencySeconds = 60)
+        {
+            _config = new MonitorConfig(frequencySeconds);
+            StartMonitor();
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MonitorService"/> class.</summary>
+        /// <param name="logger">The logger.</param>
+        public MonitorService(ILogger<MonitorService> logger)
+        {
+            _config = new MonitorConfig();
+            _logger = logger;
+
+            StartMonitor();
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MonitorService"/> class.</summary>
+        /// <param name="config">Monitor configuration.</param>
+        /// <param name="logger">The logger.</param>
+        public MonitorService(MonitorConfig config, ILogger<MonitorService> logger)
+        {
+            if (config == null)
+                _config = new MonitorConfig();
+
+            _logger = logger;
+
+            StartMonitor();
+        }
+
+        private void StartMonitor()
+        {
+            _elapsedTime.Start();
+            var monitor = new Timer(_ =>
+            {
+                var timespan = _elapsedTime.Elapsed;
+                _logger?.LogDebug($"{AppDomain.CurrentDomain.FriendlyName} running time: {timespan:dd} day(s) {timespan:hh}:{timespan:mm}:{timespan:ss}.{timespan:fff}");
+                BackgroundTimerTick?.Invoke(timespan);
+            }, null, TimeSpan.FromSeconds(_config.MonitorFrequencySeconds), TimeSpan.FromSeconds(_config.MonitorFrequencySeconds));
+        }
+    }
+}
---
+++ src/Tests/MonitorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cloud.Core.Services;
+using Cloud.Core.Testing;
+using FluentAssertions;
+using Xunit;
+
+namespace Cloud.Core.Tests
+{
+    [IsUnit]
+    public class MonitorTest
+    {
+        /// <summary>Ensure conflict exception type is captured.</summary>
+        [Fact]
+        public async void Test_ConflictException_WithMessage()
+        {
+            // Arrange.
+            var hasTicked = false;
+            TimeSpan? timeTicked = null;
+            var monitor = new MonitorService(3);
+            
+            // Act.
+            monitor.BackgroundTimerTick += (elapsed) => {
+                hasTicked = true;
+                if (timeTicked == null)
+                    timeTicked = elapsed;
+            };
+
+            await Task.Delay(3000);
+
+            // Assert.
+            monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
+            hasTicked.Should().BeTrue();
+            timeTicked.Value.Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(3));
+        }
+    }
+}
