a4cdab7e5c07d5289a9eedf6314f1c00c974a5df
Robert McCabe <rmccabe@eshopworld.com>
2020-07-02 01:11:41 +0100

    Increased test coverage

--- src/Tests/MonitorTest.cs
+++ src/Tests/MonitorTest.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Cloud.Core.Services;
 using Cloud.Core.Testing;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Cloud.Core.Tests
@@ -10,9 +12,9 @@ namespace Cloud.Core.Tests
     [IsUnit]
     public class MonitorTest
     {
-        /// <summary>Ensure conflict exception type is captured.</summary>
+        /// <summary>Ensure monitor is created with int constuctor.</summary>
         [Fact]
-        public async Task Test_ConflictException_WithMessage()
+        public async Task Test_MonitorTimer_IntConsturctor()
         {
             // Arrange.
             var monitor = new MonitorService(1);
@@ -23,6 +25,73 @@ namespace Cloud.Core.Tests
             };
 
             await Task.Delay(3000);
+
+            // Assert.
+            monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        /// <summary>Ensure monitor is created with config and logger constuctor.</summary>
+        [Fact]
+        public async Task Test_MonitorTimer_ConfigAndLoggerConstructor()
+        {
+            // Arrange.
+            var mockLogger = new Mock<ILogger<MonitorService>>();
+            var monitor = new MonitorService(new MonitorConfig(1), mockLogger.Object);
+
+            // Act.
+            monitor.BackgroundTimerTick += (elapsed) => {
+                elapsed.Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(3));
+            };
+
+            await Task.Delay(3000);
+
+            // Assert.
+            monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        /// <summary>Ensure monitor is created with config constuctor.</summary>
+        [Fact]
+        public async Task Test_MonitorTimer_ConfigConstructor()
+        {
+            // Arrange.
+            var mockLogger = new Mock<ILogger<MonitorService>>();
+            var monitor = new MonitorService(new MonitorConfig(1), null);
+
+            // Act.
+            monitor.BackgroundTimerTick += (elapsed) => {
+                elapsed.Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(3));
+            };
+
+            await Task.Delay(3000);
+
+            // Assert.
+            monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        /// <summary>Ensure monitor is created with both null constuctor.</summary>
+        [Fact]
+        public async Task Test_MonitorTimer_NullsConstructor()
+        {
+            // Arrange.
+            var monitor = new MonitorService(null, null);
+
+            // Act.
+            monitor.BackgroundTimerTick += (elapsed) => {
+                elapsed.Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(3));
+            };
+
+            await Task.Delay(3000);
+
+            // Assert.
+            monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        /// <summary>Ensure monitor is created with no constuctor.</summary>
+        [Fact]
+        public void Test_MonitorTimer_NoConstructor()
+        {
+            // Arrange.
+            var monitor = new MonitorService();
 
             // Assert.
             monitor.AppName.Should().Be(AppDomain.CurrentDomain.FriendlyName);
---
+++ src/Tests/ServiceCollectionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Cloud.Core.Testing;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Cloud.Core.Tests
+{
+    [IsUnit]
+    public class ServiceCollectionExtensionsTest
+    {
+        /// <summary>Check the string is converted into stream and back into string as expected.</summary>
+        [Fact]
+        public async void Test_ServiceCollectionExtension_AddNamedInstance()
+        {
+            // Arrange/Act.
+            var services = new ServiceCollection();
+            services.AddFactoryIfNotAdded<IBlobStorage>();
+            var exists = services.ContainsService(typeof(NamedInstanceFactory<IBlobStorage>));
+            var doestNotExist = services.ContainsService(typeof(NamedInstanceFactory<IReactiveMessenger>));
+
+            // Assert.
+            exists.Should().BeTrue();
+            doestNotExist.Should().BeFalse();
+        }
+    }
+}
