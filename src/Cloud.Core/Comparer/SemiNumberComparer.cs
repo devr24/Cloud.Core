8714f28c2888f176c9efad5b6c1f5f5680e77431
Robert McCabe <rmccabe@eshopworld.com>
2020-07-11 20:42:04 +0100

    Fixed following code analysis

--- src/Cloud.Core/Comparer/SemiNumberComparer.cs
+++ src/Cloud.Core/Comparer/SemiNumberComparer.cs
@@ -5,7 +5,6 @@ namespace Cloud.Core.Comparer
     using System;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Linq;
     using System.Text.RegularExpressions;
 
     /// <summary>
