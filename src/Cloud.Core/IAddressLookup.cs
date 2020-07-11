c4b5c78d1a46a7f5f1ffbcee8d7e183cce1a0e97
Robert McCabe <rmccabe@eshopworld.com>
2020-07-11 18:29:01 +0100

    Added xml comments to address

--- src/Cloud.Core/IAddressLookup.cs
+++ src/Cloud.Core/IAddressLookup.cs
@@ -48,38 +48,55 @@
     /// <summary>Interface IAddress containing a single addresses details.</summary>
     public interface IAddress
     {
+        /// <summary>Address in a single displayable format.</summary>
         string DisplayAddress { get; set; }
 
+        /// <summary>Main address.</summary>
         string AddressLine1 { get; set; }
 
+        /// <summary>Array of address parts formatted by address line number.</summary>
         List<string> FormattedAddress { get; set; }
 
+        /// <summary>Thoroughfare for the address.</summary>
         string Thoroughfare { get; set; }
 
-        string BuildingName { get; set; }
-
-        string SubBuildingName { get; set; }
-
-        string SubBuildingNumber { get; set; }
-
+        /// <summary>Building number.</summary>
         string BuildingNumber { get; set; }
 
+        /// <summary>Building name.</summary>
+        string BuildingName { get; set; }
+
+        /// <summary>Sub building number.</summary>
+        string SubBuildingNumber { get; set; }
+
+        /// <summary>Sub building name.</summary>
+        string SubBuildingName { get; set; }
+
+        /// <summary>First line of address.</summary>
         string Line1 { get; set; }
 
+        /// <summary>Second line of address.</summary>
         string Line2 { get; set; }
 
+        /// <summary>Third line of address.</summary>
         string Line3 { get; set; }
 
+        /// <summary>Fourth line of address.</summary>
         string Line4 { get; set; }
 
+        /// <summary>Address locality.</summary>
         string Locality { get; set; }
 
+        /// <summary>Town or city the address resides within.</summary>
         string TownOrCity { get; set; }
 
+        /// <summary>The county the address resides within.</summary>
         string County { get; set; }
 
+        /// <summary>District the address resides within.</summary>
         string District { get; set; }
 
+        /// <summary>Country the address resides within.</summary>
         string Country { get; set; }
     }
 }
