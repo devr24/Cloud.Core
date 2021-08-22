2f81305277f99c3c78675d5ac6d62eda8ff0bfed
robert.mccabe@outlook.com <robert.mccabe@outlook.com>
2021-08-22 01:55:02 +0100

    Updated email send result

--- src/Cloud.Core/IEmailProvider.cs
+++ src/Cloud.Core/IEmailProvider.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
     using System.IO;
+    using System.Net;
     using System.Threading.Tasks;
 
     /// <summary>Interface for Email notification providers.</summary>
@@ -23,13 +24,23 @@
         /// <param name="email">The email to send.</param>
         /// <returns>Task&lt;System.Boolean&gt;. <c>True</c> if sent successfully, <c>false</c> otherwise.</returns>
         /// <exception cref="Exceptions.RequestFailedException{T}">Error during processing.</exception>
-        Task<bool> SendAsync(EmailMessage email);
+        Task<EmailSendResult> SendAsync(EmailMessage email);
 
         /// <summary>Sends an email asynchronously.</summary>
         /// <param name="templatedEmail">The templated email to send.</param>
         /// <returns><c>True</c> if sent successfully, <c>false</c> otherwise.</returns>
         /// <exception cref="Exceptions.RequestFailedException{T}">Error during processing.</exception>
-        Task<bool> SendAsync(EmailTemplateMessage templatedEmail);
+        Task<EmailSendResult> SendAsync(EmailTemplateMessage templatedEmail);
+    }
+
+    /// <summary>Email send result.</summary>
+    public class EmailSendResult
+    {
+        /// <summary>Success [true] or failure [false].</summary>
+        public bool Success { get; set; }
+
+        /// <summary>Error message if failed to send.</summary>
+        public string ErrorMessage { get; set; }
     }
 
     /// <summary>Email message.</summary>
